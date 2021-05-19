b87cea62bf03626429619ebcea6d80689395a566
El Mehdi ASSALI <assalielmehdi@gmail.com>
2021-05-19 23:25:43 +0200

    Add findById unit test (#1)
    * Add findById unit test
    
    * Code review changes

---
+++ src/test/java/ma/ensiasit/ensias/project_tracking/services/TaskServiceImplTest.java
@@ -0,0 +1,41 @@
+package ma.ensiasit.ensias.project_tracking.services;
+
+import ma.ensiasit.ensias.project_tracking.models.Task;
+import ma.ensiasit.ensias.project_tracking.repositories.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TaskServiceImplTest {
+
+  @Mock
+  private TaskRepository taskRepository;
+
+  @InjectMocks
+  private TaskServiceImpl taskService;
+
+  @Test
+  void givenExistingTaskShouldReturnCorrectTask() {
+    // Given
+    String id = "id-1";
+
+    when(taskRepository.findById(id)).thenReturn(Optional.of(new Task(id, null, null)));
+
+    // When
+    var returnedTask = taskService.getTask(id);
+
+    // Then
+    verify(taskRepository).findById(id);
+    assertEquals(id, returnedTask.getId());
+  }
+
+}
